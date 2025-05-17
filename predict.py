c44e48cda86ea37d790e2b31d62c3b03cab405e4
0verdoz <wmakpodo@st.ug.edu.gh>
2025-05-17 19:55:08 +0100

    refactor: removed location and fixed model overfitting error

--- predict.py
+++ predict.py
@@ -1,21 +1,32 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas as pd
 
+main_brands = ['Samsung','Apple','Other','Huawei','Google','LG']
+main_models = ['iPhone X','iPhone 8 Plus','Galaxy S6 edge','Galaxy S7 edge','iPhone XR','iPhone 7 Plus','Galaxy S21 Ultra']
 
+def clean_brand(brand):
+    return brand if brand in main_brands else 'Others'
+
+def clean_model(model):
+    return model if model in main_models else 'Others'
 
 def load_model():
-    with open('save_steps.pkl', 'rb') as file:
+    with open('prices.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
 
 data = load_model()
 
-regressor = data["model"]
-ohe = data["ohe"]
-te_model = data["te_model"]
-le_location = data["le_location"]
+xgb_model = data["model"]
+le_brand = data["le_brand"]
+le_model = data["le_model"]
+
+# The Encoders below cause errors.
+# regressor = data["model"]
+# ohe = data["ohe"]
+# te_model = data["te_model"]   
+# le_location = data["le_location"]
 # brand_encoded = data["brand_encoded"]
 # model_encoded = data["model_encoded"]
 # location_encoded = data["location_encoded"]
@@ -24,58 +35,59 @@ le_location = data["le_location"]
 def show_predict_page():
     st.title("📱 Mobile Price Predictor.")
 
-    brands = (
-        "Samsung",
-        "Apple",
-        "Huawei",
-        "Google",
-        "LG",
-        "Tecno",
-        "Infinix",
-        "Itel",
-        "Oppo",
-        "Xiaomi",
-        "Nokia",
-        "Motorola",
-        "Vivo",
-        "Realme",
-        "OnePlus",
-        "HTC",
-        "X-Tigi",
-        "Honor",
-    )
+    # brands = (
+    #     "Samsung",
+    #     "Apple",
+    #     "Huawei",
+    #     "Google",
+    #     "LG",
+    #     "Tecno",
+    #     "Infinix",
+    #     "Itel",
+    #     "Oppo",
+    #     "Xiaomi",
+    #     "Nokia",
+    #     "Motorola",
+    #     "Vivo",
+    #     "Realme",
+    #     "OnePlus",
+    #     "HTC",
+    #     "X-Tigi",
+    #     "Honor",
+    # )
 
-    storage_options = (
-        64,
-        128,
-        256,
-        512,
-        1024,
-    )
+    # storage_options = (
+    #     64,
+    #     128,
+    #     256,
+    #     512,
+    #     1024,
+    # )
 
     # User Inputs
-    brand = st.selectbox("Brands", brands)
+    brand = st.text_input("Brands", value="Samsung")
     model = st.text_input("Enter Model (e.g., Galaxy S20 Ultra)", value="Galaxy S20 Ultra")
-    storages = st.select_slider("Phone Storage (GB)", storage_options)
-    location = st.text_input("Enter your location (e.g., Tema)", value="Tema")
+    storage = st.number_input("Phone Storage (GB)")
+    # location = st.text_input("Enter your location (e.g., Tema)", value="Tema")
 
-    ok = st.button("Display Price")
+    ok = st.button("Predict Price")
+
     if ok:
-        x = np.array([[brand, model, location, storages]])
+        # x = np.array([[brand, model, location, storages]])
+        brand_cleaned = clean_brand(brand)
+        model_cleaned = clean_model(model)
 
-        brand_input = pd.DataFrame(x[:, 0], columns=['brand'])
-        brand_encoded = ohe.transform(brand_input).toarray()
+        try:
+          encoded_brand = le_brand.transform([brand_cleaned])[0]
+          encoded_model = le_model.transform([model_cleaned])[0]
+        except ValueError:
+            st.error("Nan")
+            return
 
-        model_input = pd.Series(x[:, 1], name='model')  
-        model_encoded = te_model.transform(model_input).to_numpy().reshape(-1, 1)
 
-        location_input = pd.Series(x[:, 2])
-        location_encoded = le_location.transform(location_input).reshape(-1, 1)
+        x = np.array([[encoded_brand, encoded_model, storage]])
 
-        storage_input = np.array([[x[0, 3]]], dtype=float)
-        x = np.hstack([brand_encoded, model_encoded, location_encoded, storage_input])
-
-        price = regressor.predict(x)
+        price = xgb_model.predict(x)
         st.subheader(f"The estimated price is Ghs(¢){price[0]:.2f}")
 
 
